a73cc5b790ad14b276a5c5aad2fcb0863fb863dd
UTKARSH SINGH <utkarshsinghthakur1476@gmail.com>
2023-09-20 21:07:34 +0530

    commit2

---
+++ JAVASCRIPT 1/test.js
@@ -0,0 +1,58 @@
+// console.log("Utkarsh")
+
+// const number1 = 12345;
+// let city = "Ayodhya"
+// var country = "Russia"
+// place = "Srinagar"
+
+// console.log(city)
+// console.log(country)
+// console.log(place)
+
+// console.log("\n")
+
+// console.table([city,country,place])
+
+// let score = "33aa"
+// console.log(score)
+// console.log(typeof score)
+
+// let inNumber = Number(score)
+// console.log(typeof inNumber)
+// console.log(inNumber)
+
+// let value = 0
+// let isValue = Boolean(value)
+// console.log(isValue)
+
+// const myFunction = function()
+// {
+//     console.log("Hello World")
+// }
+
+// myFunction();
+
+// +++++++++++++++++++++++++++++++++++++ STRING ++++++++++++++++++++++++++++++++
+
+/*const str1 = "Utkarsh"
+console.log(str1)
+const str = "Singh"
+
+console.log(`Hello my name is ${str1} and my surname is ${str}`);
+
+const str2 = new String ('Utkarsh-is-here')
+console.log(str2)
+console.log(str2.length)
+console.log(str1[0])
+console.log(str1.__proto__)
+console.log(str1.toUpperCase())
+console.log(str1.charAt(3))
+console.log(str1.indexOf('s'))
+
+const newStr = str1.slice(-1,)
+console.log(newStr) 
+
+const newStr1 = "Hello ji"
+console.log(newStr1.includes("ji"))*/
+
+// +++++++++++++++++++++++++++++++++++++ MATH +++++++++++++++++++++++++++++++++
